7d6793f2b32b8e4fa544a27595646164270d9b2c
Tomisin <oyemadeoluwatomisin75@gmail.com>
2025-08-29 08:26:11 +0100

    ...

--- src/components/Product/Hero.tsx
+++ src/components/Product/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export const Hero = () => {
   return (
-    <section className='-tracking-[0.04em] md:mb-[500px] bg-primary-light py-28 lg:py-52 md:h-[40lvh] bg-service-bg'>
+    <section className='-tracking-[0.04em] md:mb-[500px] bg-primary-light py-28 lg:py-52 md:h-[60lvh] 2xl:h-[40lvh] bg-service-bg'>
       <div className='container'>
         <div className="max-w-[538px] mx-auto text-center">
           <h2 className='text-dark text-[30px] md:text-[60px] font-medium md:leading-[100%]'>
