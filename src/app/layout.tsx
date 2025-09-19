43ecdf84c4152d3433e1ede0f4153b1e087bc39b
Tomisin <oyemadeoluwatomisin75@gmail.com>
2025-09-19 11:30:21 +0100

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist } from "next/font/google"
-import "./globals.css"
+import "./globals.scss"
 
 const geistSans = Geist({
   subsets: ["latin"],
